46fab4359424ef1be5f8e2deed5a024ac8496677
MateuszS04 <mateuszsondej04@gmail.com>
2024-12-19 20:17:49 +0100

    Logging view with database in sqlite

---
+++ demo1/src/main/java/data_base/DatabaseConnection.java
@@ -0,0 +1,33 @@
+package data_base;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public Connection databaseLink;
+
+    public Connection getConnection() {
+        String URL="jdbc:sqlite:C://Users//mateu//IdeaProjects//project_po//demo1//login_data_base.sqlite";
+        try{
+            Class.forName("org.sqlite.JDBC");
+            databaseLink = DriverManager.getConnection(URL);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class not found");
+            e.printStackTrace();
+        }catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("SQL Exception");
+        }
+        return databaseLink;
+    }
+    public void closeConnection() {
+        if (databaseLink != null) {
+            try{
+                databaseLink.close();
+            }catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Failed to close connection");
+            }
+        }
+    }
+}
---
+++ projekt/src/main/java/users.java
@@ -0,0 +1,28 @@
+package Bazy_danych;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Obiekty.Klient;
+
+public class users {
+    public static final String Driver = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:sqlite:baza_danych_kont.sqlite";
+
+    private Connection conn;
+    private Statement stmt;
+
+    public users() {
+        try {
+            conn = DriverManager.getConnection(URL);
+            stmt = conn.createStatement();
+        } catch (SQLException e) {
+            System.err.println("Problem with connecting to database");
+            e.printStackTrace();
+        }
+    }
+
+
+}
